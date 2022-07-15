e5f2526edcf9591d85505be53fd43b560b4780a6
Gilberto Alexandre dos Santos <gilberto.alexsantos@gmail.com>
2022-07-16 01:09:16 +0200

    remove sdk responsibility to bind classinstaller

--- Assets/_Scripts/Sample/Scripts/GameStartCustomScriptableInstaller.cs
+++ Assets/_Scripts/Sample/Scripts/GameStartCustomScriptableInstaller.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using BluEngine;
 using UnityEngine;
+using Zenject;
 
 [CreateAssetMenu(menuName = "Game/Installers/GameStart", fileName = "GameStartInstaller")]
 public class GameStartCustomScriptableInstaller : AppStartCustomScriptableInstaller
 {
-    public override Type AnyTypeFromGameAssembly => typeof(GameStartCustomScriptableInstaller);
+    public override void InstallBindings(DiContainer container)
+    {
+        BindClassInstallers(container);
+    }
+
+    private void BindClassInstallers(DiContainer container)
+    {
+        Type classInstallerType = typeof(ClassInstaller);
+        IEnumerable<Type> customClassTypes = Assembly.GetAssembly(typeof(GameStartCustomScriptableInstaller)).GetTypes()
+            .Where(type => type.IsClass && !type.IsAbstract && type.IsSubclassOf(classInstallerType));
+        foreach (Type type in customClassTypes)
+        {
+            ClassInstaller installer = (ClassInstaller) Activator.CreateInstance(type);
+            installer.InstallBindings(container);
+        }
+    }
 }
--- Assets/com.bludea.bluengine/Runtime/Scripts/AppStart/AppStartCustomScriptableInstaller.cs
+++ Assets/com.bludea.bluengine/Runtime/Scripts/AppStart/AppStartCustomScriptableInstaller.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
 using Zenject;
 
@@ -9,23 +5,6 @@ namespace BluEngine
 {
     public abstract class AppStartCustomScriptableInstaller : ScriptableObject
     {
-        public abstract Type AnyTypeFromGameAssembly { get; }
-        
-        public virtual void InstallBindings(DiContainer container)
-        {
-            BindClassInstallers(container);
-        }
-
-        private void BindClassInstallers(DiContainer container)
-        {
-            Type classInstallerType = typeof(ClassInstaller);
-            IEnumerable<Type> customClassTypes = Assembly.GetAssembly(AnyTypeFromGameAssembly).GetTypes()
-                .Where(type => type.IsClass && !type.IsAbstract && type.IsSubclassOf(classInstallerType));
-            foreach (Type type in customClassTypes)
-            {
-                ClassInstaller installer = (ClassInstaller) Activator.CreateInstance(type);
-                installer.InstallBindings(container);
-            }
-        }
+        public abstract void InstallBindings(DiContainer container);
     }
 }
