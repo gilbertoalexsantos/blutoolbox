f38c19b01920dcf5166f5db2ddb5f2579eb57f61
Gilberto Alexandre dos Santos <gilberto.alexsantos@gmail.com>
2022-10-13 19:01:54 +0200

    move bind class installers to parent class

--- Assets/_Scripts/Sample/Scripts/GameStartCustomScriptableInstaller.cs
+++ Assets/_Scripts/Sample/Scripts/GameStartCustomScriptableInstaller.cs
@@ -1,28 +1,13 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using BluEngine;
 using UnityEngine;
-using Zenject;
 
 [CreateAssetMenu(menuName = "Game/Installers/GameStart", fileName = "GameStartInstaller")]
 public class GameStartCustomScriptableInstaller : AppStartCustomScriptableInstaller
 {
-    public override void InstallBindings(DiContainer container)
+    protected override IEnumerable<Assembly> CustomInstallerAssemblies => new List<Assembly>
     {
-        BindClassInstallers(container);
-    }
-
-    private void BindClassInstallers(DiContainer container)
-    {
-        Type classInstallerType = typeof(ClassInstaller);
-        IEnumerable<Type> customClassTypes = Assembly.GetAssembly(typeof(GameStartCustomScriptableInstaller)).GetTypes()
-            .Where(type => type.IsClass && !type.IsAbstract && type.IsSubclassOf(classInstallerType));
-        foreach (Type type in customClassTypes)
-        {
-            ClassInstaller installer = (ClassInstaller) Activator.CreateInstance(type);
-            installer.InstallBindings(container);
-        }
-    }
+        Assembly.GetAssembly(typeof(GameStartCustomScriptableInstaller))
+    };
 }
--- Assets/com.bludea.bluengine/Runtime/Scripts/AppStart/AppStartCustomScriptableInstaller.cs
+++ Assets/com.bludea.bluengine/Runtime/Scripts/AppStart/AppStartCustomScriptableInstaller.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using Zenject;
 
@@ -5,6 +9,26 @@ namespace BluEngine
 {
     public abstract class AppStartCustomScriptableInstaller : ScriptableObject
     {
-        public abstract void InstallBindings(DiContainer container);
+        protected virtual IEnumerable<Assembly> CustomInstallerAssemblies => Enumerable.Empty<Assembly>();
+
+        public virtual void InstallBindings(DiContainer container)
+        {
+            BindClassInstallers(container);
+        }
+
+        private void BindClassInstallers(DiContainer container)
+        {
+            Type classInstallerType = typeof(ClassInstaller);
+            foreach (Assembly assembly in CustomInstallerAssemblies)
+            {
+                IEnumerable<Type> customClassTypes = assembly.GetTypes()
+                    .Where(type => type.IsClass && !type.IsAbstract && type.IsSubclassOf(classInstallerType));
+                foreach (Type type in customClassTypes)
+                {
+                    ClassInstaller installer = (ClassInstaller) Activator.CreateInstance(type);
+                    installer.InstallBindings(container);
+                }   
+            }
+        }
     }
 }
